54a89ab8e2418b699713e9b241f5ce82f9628ffa
Danteron27 <jhonatanagosto1@hotmail.com>
2022-06-24 13:52:40 -0500

    version 1

---
+++ prueba.php
@@ -0,0 +1,40 @@
+<?php
+    
+    if (isset($_REQUEST['add'])){
+        $hostDB = 'localhost:8081';
+    $nameDB = 'prueba';
+
+    $host = "mysql:host=$hostDB;dbname=$nameDB";
+    $user = 'root';
+    $pass = '';
+
+    $conn = new PDO ($host, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+        $prueba = isset($_REQUEST['prueba']) ? $_REQUEST['prueba'] : null;
+        $id = isset($_REQUEST['id']) ? $_REQUEST['id'] : null;
+        if($id == null){
+            $id = $productos->rowCount() + 1;
+        }
+        $insert = "INSERT INTO uwu VALUES ('$id','$prueba');";
+        $conn->exec($insert);
+        echo 'AWA';
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <input type="checkbox" name="prueba[]" value="1">
+        <input type="submit" value="UWU" name="add">
+    </form>
+</body>
+</html>
